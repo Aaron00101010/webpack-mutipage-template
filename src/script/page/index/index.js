ce81d446bfc8e49681028bea783bbd405c1dd3ab
xieqiang <xieqiang9408@gmail.com>
2018-08-09 09:20:43 +0800

    update:init

--- src/script/page/index/index.js
+++ src/script/page/index/index.js
@@ -1,4 +1,4 @@
-import '@/style/common/common';
-import '@/style/page/index/index';
+import '@/style/common/common'
+import '@/style/page/index/index'
 
-$(document.body).animate({ 'padding-top': '20px' }, 2000);
+$(document.body).animate({ 'padding-top': '20px' }, 2000)
